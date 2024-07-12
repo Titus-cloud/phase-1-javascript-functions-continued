7e0e80c6c245dcc7ea8c71d2f12f87cdd0635782
titus-cloud <titus.onzere@student.moringaschool.com>
2024-07-12 15:59:46 +0300

    My javascript function continued lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+require('./helpers.js');
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log('saturdayFun:', saturdayFun);
+
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+
+function wrapAdjective(wrapper) {
+  return function(adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
+
